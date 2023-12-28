0800a9e5666f83ef79eb64cc39b369f9d43a954a
Joytunnahar <joytunnahar@gmail.com>
2023-12-28 17:11:24 +0600

    basic setuo and dasyui add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className="text-4xl font-bold text-center ">Hello Hello Hello!!!!!!</h1>
+     
+      
+     
+    </>
+  )
+}
+
+export default App
